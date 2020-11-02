2e4fbec3f71529c7f5520ce3ddafa9d1ffb7c8c0
Rafael Madakis <rafaelmadakis@gmail.com>
2020-11-01 22:53:04 -0300

    toast

--- app/src/main/java/com/example/toastproject/MainActivity.java
+++ app/src/main/java/com/example/toastproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.toastproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirToast(View view){
+
+        /**
+         * Criação do toast
+         */
+//        Toast.makeText(getApplicationContext(), "Botão pressionado", Toast.LENGTH_LONG).show();
+
+        ImageView imagem = new ImageView(getApplicationContext());
+        imagem.setImageResource(android.R.drawable.star_big_off);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(imagem);
+        toast.show();
+
+
     }
 }
